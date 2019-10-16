d4c48e8ec4c62e179800363adc646ff783f23547
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2019-10-16 22:33:15 +0530

    [nodeServer] Add initialValues
    For currencies

---
+++ nodeServer/src/initialValues.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { label: 'USD', value: 1 },
+  { label: 'EUR', value: 1.09883 },
+  { label: 'JPY', value: 0.00936 },
+  { label: 'GBP', value: 1.22222 },
+  { label: 'CAD', value: 0.75078 },
+  { label: 'INR', value: 0.01406 },
+];
